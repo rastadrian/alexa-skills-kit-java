b7627d4482299c1190c450c3d59e1b8ec7088f5a
Adrian Pena <rastadrian@gmail.com>
2017-05-22 11:59:33 -0400

    Removes System builder due to invalid deserialization

--- src/com/amazon/speech/speechlet/System.java
+++ src/com/amazon/speech/speechlet/System.java
@@ -4,65 +4,22 @@ package com.amazon.speech.speechlet;
  * Context System for Session-less requests.
  */
 public class System {
-    private final Application application;
-    private final User user;
-    private final String apiEndpoint;
-
-    /**
-     * Returns a new builder instance used to construct a new {@code System}.
-     *
-     * @return the builder
-     */
-    public static Builder builder() {
-        return new Builder();
-    }
-
-    private System(Builder builder) {
-        this.application = builder.application;
-        this.user = builder.user;
-        this.apiEndpoint = builder.apiEndpoint;
-    }
-
-    public Application getApplication() {
-        return application;
-    }
+    private User user;
+    private String apiEndpoint;
 
     public User getUser() {
         return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 
     public String getApiEndpoint() {
         return apiEndpoint;
     }
 
-    /**
-     * Builder used to construct a new {@code System}.
-     */
-    public static final class Builder {
-        private Application application;
-        private User user;
-        private String apiEndpoint;
-
-        private Builder() {
-        }
-
-        public Builder withApplication(final Application application) {
-            this.application = application;
-            return this;
-        }
-
-        public Builder withUser(final User user) {
-            this.user = user;
-            return this;
-        }
-
-        public Builder withApiEndpoint(final String apiEndpoint) {
-            this.apiEndpoint = apiEndpoint;
-            return this;
-        }
-
-        public System build() {
-            return new System(this);
-        }
+    public void setApiEndpoint(String apiEndpoint) {
+        this.apiEndpoint = apiEndpoint;
     }
 }
